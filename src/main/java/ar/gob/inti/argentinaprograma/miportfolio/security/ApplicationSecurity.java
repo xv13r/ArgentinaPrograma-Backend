60efeaee8d8e0ef4f1bc1cecf3df63ddb13c829e
xv13r <44001932+xv13r@users.noreply.github.com>
2022-08-23 12:10:27 -0300

    Update ApplicationSecurity.java

--- src/main/java/ar/gob/inti/argentinaprograma/miportfolio/security/ApplicationSecurity.java
+++ src/main/java/ar/gob/inti/argentinaprograma/miportfolio/security/ApplicationSecurity.java
@@ -105,13 +105,13 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of(allowedOrigins));
-		config.addAllowedHeader(List.of("Origin", "Access-Control-Allow-Origin", "Content-Type",
+		config.setAllowedOrigins(Arrays.asList(allowedOrigins));
+		config.addAllowedHeader(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		config.setExposedHeaders(List.of("Origin", "Content-Type", "Accept", "Authorization",
+		config.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
                 "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setMaxAge(MAX_AGE_SECS);
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
